b5231854ab728e17c2b9d705ddc4d7f51ffe4731
sybercat <sybercat@163.com>
2026-05-05 23:43:10 +0300

    pass test: it cannot be moved right through other blocks

--- src/Board.mjs
+++ src/Board.mjs
@@ -93,6 +93,10 @@ export class Board {
 
     let bottomEdge = this.tetromino.location.y + this.tetromino.height - 1;
     for (let i = bottomEdge; i >= bottomEdge - this.tetromino.height + 1; i--) {
+      if(this.board[i][rightEdge + 1] !== '.') return;
+    }
+
+    for (let i = bottomEdge; i >= bottomEdge - this.tetromino.height + 1; i--) {
       for (let j = this.tetromino.location.x + this.tetromino.width - 1; j >= this.tetromino.location.x; j--) {
         this.board[i][j + 1] = this.board[i][j];
         this.board[i][j] = '.';
