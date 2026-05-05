52f3512d59d74ade89b577a3e94a18d145547036
sybercat <sybercat@163.com>
2026-05-05 18:29:50 +0300

    refactor

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,3 +1,13 @@
 export class Tetromino {
+    tetrominoArr;
+    tetrominoStr;
+    width;
+    height;
     static T_SHAPE = '.T.\nTTT';
+    constructor(symbol) {
+        this.symbol = symbol;
+        this.rows = symbol.split('\n');
+        this.width = this.rows[0].length;
+        this.height = this.rows.length;
+    }
 }
