2707eb87c3d7cb7ad4f5e300934f01b590c6f377
sybercat <sybercat@163.com>
2026-05-05 22:47:56 +0300

    pass test: a falling tetromino can be moved left

--- src/Board.mjs
+++ src/Board.mjs
@@ -67,6 +67,16 @@ export class Board {
     this.tetromino.location.y += 1;
   }
 
+  moveLeft() {
+    let bottomEdge = this.tetromino.location.y + this.tetromino.height - 1;
+    for (let i = bottomEdge; i >= bottomEdge - this.tetromino.height + 1; i--) {
+      for (let j = this.tetromino.location.x; j <= this.tetromino.location.x + this.tetromino.width - 1; j++) {
+        this.board[i][j - 1] = this.board[i][j];
+        this.board[i][j] = '.';
+      }
+    }this.tetromino.location.x -= 1;
+  }
+
   tick() {
     let bottomEdge = this.tetromino.location.y + this.tetromino.height - 1;
     if(!this.canMove()) return;
