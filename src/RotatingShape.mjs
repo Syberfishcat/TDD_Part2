df34ff03813345fa2f2aa2877adbdd77a0ee8856
sybercat <sybercat@163.com>
2026-05-05 12:42:48 +0300

    refactor

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -2,13 +2,15 @@ export class RotatingShape {
     cube;
     size;
     constructor(str) {
-        let fomatStr = [...str].filter(item => /^[a-zA-Z\n]$/.test(item)).join('') + '\n';
-        this.size = fomatStr.split('\n')[0].length;
-        this.cube = Array.from({ length: this.size }, () => new Array(this.size).fill(''));
-        let formatArr = [...fomatStr].filter(c => c !== '\n');
-        for (let i = 0; i < this.size; i++){
-            for (let j = 0; j < this.size; j++) {
-                this.cube[i][j] = formatArr[i * this.size + j];
+        if(str.map){}else{
+            let fomatStr = [...str].filter(item => /^[a-zA-Z\n]$/.test(item)).join('') + '\n';
+            this.size = fomatStr.split('\n')[0].length;
+            this.cube = Array.from({ length: this.size }, () => new Array(this.size).fill(''));
+            let formatArr = [...fomatStr].filter(c => c !== '\n');
+            for (let i = 0; i < this.size; i++){
+                for (let j = 0; j < this.size; j++) {
+                    this.cube[i][j] = formatArr[i * this.size + j];
+                }
             }
         }
     }
