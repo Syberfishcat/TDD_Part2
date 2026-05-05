f3ad00ed4e6d3b1d541cfe0e0d99779ff7f355d2
sybercat <sybercat@163.com>
2026-05-05 16:47:55 +0300

    refactor

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,12 +1,3 @@
 export class Tetromino {
-    static T_SHAPE = new Tetromino('.T.\nTTT');
-    tetromino;
-    location;
-    height;
-    width;
-    constructor(pattern){
-        this.tetromino = pattern.split('\n');
-        this.width = this.tetromino[0].length;
-        this.height = this.tetromino.length;
-    }
+    static T_SHAPE = '.T.\nTTT';
 }
