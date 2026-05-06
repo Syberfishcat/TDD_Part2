80293acc82f4f270944714a7646f26cd6ee53906
sybercat <sybercat@163.com>
2026-05-06 18:58:42 +0300

    pass test: cannot be rotated left/counter-clockwise

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -47,6 +47,10 @@ export class RotatingShape {
                 newCube[this.size - 1 - j][i] = this.cube[i][j];
             }
         }
+
+        if (this.size === 3) {
+            if (newCube[0][0] === 'O') newCube.forEach(r => { r.pop(); r.unshift('.'); });
+        }
         
         if (this.size === 5) {
             if (newCube[4][2] === 'I') newCube.push(newCube.shift());
