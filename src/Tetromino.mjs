640d4f7bd2ecf99397145db0271f2f4794613e8e
sybercat <sybercat@163.com>
2026-05-06 12:53:04 +0300

    refactor

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,7 +1,8 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
-    static T_SHAPE = RotatingShape.fromString(`.T.
+    static T_SHAPE = RotatingShape.fromString(
+        `.T.
          TTT
          ...`);
 
