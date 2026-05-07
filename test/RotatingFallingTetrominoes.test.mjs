f0e6bb9c0d72399e5d57d81e69e642c5435fa59d
sybercat <sybercat@163.com>
2026-05-07 17:44:26 +0300

    add and pass test: it cannot be rotated right when there is no room (blocked by other blocks)

--- test/RotatingFallingTetrominoes.test.mjs
+++ test/RotatingFallingTetrominoes.test.mjs
@@ -114,5 +114,25 @@ describe("Rotating falling tetrominoes", () => {
                  ..........`
             );
         })
+
+        test("it cannot be rotated right when there is no room (blocked by other blocks)", () => {
+            board.drop(Tetromino.I_SHAPE);
+            fallToBottom(board);
+            board.drop(Tetromino.I_SHAPE);
+            board.tick();
+            board.tick();
+            board.tick();
+            board.tick();
+            board.rotateRight();
+            
+            expect(board.toString()).to.equalShape(
+                `..........
+                 ..........
+                 ..........
+                 ..........
+                 ..IIII....
+                 ..IIII....`
+            );
+        })
     })
 })
